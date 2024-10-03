c0f966221b7bc8e3ab9c18c0fb38f6e126dff536
ElCuervas <151858255+ElCuervas@users.noreply.github.com>
2024-10-03 19:37:43 -0300

    Carga de proyecto
    Base de proyecto n-migos con springboot

---
+++ src/main/java/com/company/n_migos/NMigosApplication.java
@@ -0,0 +1,13 @@
+package com.company.n_migos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NMigosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NMigosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/n_migos/NMigosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.n_migos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NMigosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
