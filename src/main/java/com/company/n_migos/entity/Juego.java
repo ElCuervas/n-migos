23d0d210d19f69136f51781970d4c0da21b68c79
ElCuervas <sebastianaillapan017@gmail.com>
2024-12-26 12:42:55 -0300

    refactor: Pequeño refactori en Juego.java
    -Se borraron los metodos getter y setter, remplazandolo con la etiqueta @Builder de Lombok

--- src/main/java/com/company/n_migos/entity/Juego.java
+++ src/main/java/com/company/n_migos/entity/Juego.java
@@ -1,17 +1,15 @@
 package com.company.n_migos.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 import java.io.Serializable;
 import java.util.Date;
 
 @Data
-@AllArgsConstructor
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 @ToString
 @Entity
 @Table(name="juego")
@@ -38,52 +36,4 @@ public class Juego implements Serializable {
     private Float calificacion;
 
 
-    //los getters y setters están manual por un conflicto sin resolver con lombok al compilar
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public Date getLanzamiento() {
-        return lanzamiento;
-    }
-
-    public void setLanzamiento(Date lanzamiento) {
-        this.lanzamiento = lanzamiento;
-    }
-
-    public String getImagen() {
-        return imagen;
-    }
-
-    public void setImagen(String imagen) {
-        this.imagen = imagen;
-    }
-
-    public String getSinapsis() {
-        return sinapsis;
-    }
-
-    public void setSinapsis(String sinapsis) {
-        this.sinapsis = sinapsis;
-    }
-
-    public Float getCalificacion() {
-        return calificacion;
-    }
-
-    public void setCalificacion(Float calificacion) {
-        this.calificacion = calificacion;
-    }
 }
